456e569f94ef8a5450d676db9f1502e23627d11e
ShubhamDhanani <70098570+ShubhamDhanani@users.noreply.github.com>
2020-10-01 09:58:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+#amazing project
+# JS CraftCamp - Website
+
+_New README is WIP_
+
+### Start developing:
+
+1. clone
+2. install dependencies
+
+### Scripts
+
+- `npm run storybook` for storybook
+- `npm start` for gatsby dev
+
+### Create new components
+
+```
+npm run hygen component new --name <COMPONENT_NAME>
+```
+
+The prompt will ask for do you want to create a:
+
+- component
+- section
+
+A section basically a part of a page, e.g. the "child care" is a section.
+
+### Cool packages
+
+- https://www.npmjs.com/package/sass-mq
+  > makes it easier to write media-queries
+- https://www.npmjs.com/package/@storybook/addon-viewport
+  > Change the viewport in the storybook preview
+- http://www.hygen.io/
+  > Some generator to make it easier creating components with all depending files
---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  siteMetadata: {
+    title: "JSCraftCamp - 2020 in Munich",
+    description: "Website for JSCraftCamp - 2020 in Munich",
+    author: "@davelosert, @rhosts, @michaelzoidl, @Narigo",
+  },
+  pathPrefix: "/",
+  plugins: [
+    "gatsby-transformer-json",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-svg-sprite",
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        data: `
+          @import "node_modules/sass-mq/_mq.scss";
+          @import "${__dirname}/src/config.scss";
+        `,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "JSCraftCamp - 2020 in Munich",
+        short_name: "JSCraftCamp",
+        start_url: "/",
+        background_color: "#ececec",
+        theme_color: "#ececec",
+        display: "minimal-ui",
+        icon: "src/assets/images/logo.png",
+      },
+    },
+  ],
+}
